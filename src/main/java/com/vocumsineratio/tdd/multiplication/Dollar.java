cc028096ff34bc4fff7f76a7abb422bf5d7496df
Danil <danil@vast.com>
2018-10-22 09:39:27 -0400

    GREEN: simplest thing that could possibly work

--- src/main/java/com/vocumsineratio/tdd/multiplication/Dollar.java
+++ src/main/java/com/vocumsineratio/tdd/multiplication/Dollar.java
@@ -4,13 +4,12 @@ package com.vocumsineratio.tdd.multiplication;
  * @author Danil Suits (danil@vast.com)
  */
 class Dollar {
-    public int amount;
+    public int amount = 10;
 
     public Dollar(int i) {
         //TODO: To change body of created methods use File | Settings | File Templates.
     }
 
     public void times(int i) {
-        //TODO: To change body of created methods use File | Settings | File Templates.
     }
 }
