89cdd1fca54cd83c0b7a5e749bb33eb3ce632c27
Danil <danil@vast.com>
2018-10-22 09:40:56 -0400

    GREEN: remove duplication

--- src/main/java/com/vocumsineratio/tdd/multiplication/Dollar.java
+++ src/main/java/com/vocumsineratio/tdd/multiplication/Dollar.java
@@ -4,7 +4,7 @@ package com.vocumsineratio.tdd.multiplication;
  * @author Danil Suits (danil@vast.com)
  */
 class Dollar {
-    public int amount = 10;
+    public int amount = 5 * 2;
 
     public Dollar(int i) {
         //TODO: To change body of created methods use File | Settings | File Templates.
